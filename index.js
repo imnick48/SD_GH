f5537d912d835a7a85478e02889bd851574a0519
Sagnick Das <45506982+imnick48@users.noreply.github.com>
2025-03-31 09:55:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV Sagnick Here')
+  res.send('Welcome Sagnick')
 })
 
 app.get('/courses', function (req, res) {
